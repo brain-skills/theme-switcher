de15cae8fc119e288dc84c8d2031e012b149dea4
smart-skill <web.pr@mail.ru>
2023-04-24 06:41:51 +0400

    init version 0.0.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,12 @@
-// Выбираем кнопку
-const btn = document.querySelector(".btn-toggle");
-// Выбираем настройки темы из localStorage
-const currentTheme = localStorage.getItem("theme");
-
-// Если текущая тема в localStorage "dark"...
-if (currentTheme == "dark") {
-  // ...тогда используем класс .dark-theme
-  document.body.classList.add("dark-theme");
-}
-
-// Слушаем нажатия на кнопку 
-btn.addEventListener("click", function(event) {
-  // Переключаем класс .dark-theme при каждом нажатии
-  document.body.classList.toggle("dark-theme");
-  
-  // По умолчанию в настройках зададим светлую тему
-  let theme = "light";
-  // Если установлен класс .dark-theme...
-  if (document.body.classList.contains("dark-theme")) {
-    // ...тогда в настройках установим темную тему
-    theme = "dark";
-    event.target.textContent = 'Light Theme';
-  } else {
-    event.target.textContent = 'Dark Theme';
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".btn-toggle");
+  let currentTheme = localStorage.getItem("theme");
+  if (currentTheme == "dark") {
+      document.body.classList.add("dark-theme");
   }
-  // Сохраняем настройки в хранилище localStorage
-  localStorage.setItem("theme", theme);
-  event.preventDefault();
+  btn.addEventListener("click", function(event) {
+      document.body.classList.toggle("dark-theme");
+      currentTheme = currentTheme == "dark" ? "light" : "dark";
+      localStorage.setItem("theme", currentTheme);
+  });
 });
